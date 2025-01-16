e927e363e305d75dc31290f6c47951bd9db22789
Mallik0715 <mallikarjun0715@gmail.com>
2025-01-16 22:32:13 +0530

    changes updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React,{useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/KarunakarUsala_Resume.pdf";
-import pdf from "../../Assets/../Assets/MallikarjunMilkuri_Resume.pdf";
+import pdf from "../../Assets/../Assets/Mallikarjun_Resume.pdf";
+// import pdf from "../../Assets/..Assets/Mallikarjun_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
